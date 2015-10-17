164ca06df590e8158e87b1d7000b476a3e91d1ec
Harry Parton <hparton@hotmail.co.uk>
2015-10-17 19:25:27 +0100

    Clean up some of the formatting and add overwrite warning.

--- index.js
+++ index.js
@@ -9,96 +9,85 @@ var colors = require('colors');
 var modulesLocal = '/project_modules/';
 
 var gruntQuestions = [
-  {
+{
     type: "input",
     name: "project",
     message: "Project Name"
-  },
-  {
+},
+{
     type: "input",
     name: "version",
     message: "Version",
-    default: function () { return "1.0.0"; }
-  },
-  {
+    default: function () {
+        return "1.0.0";
+    }
+},
+{
     type: "input",
     name: "description",
     message: "Project Description"
-  }
-];
-console.log('')
-console.log('Current directory: ' + process.cwd());
-console.log('')
+}];
+
+createTitle('Current directory: ' + process.cwd());
 
 inquirer.prompt([
-  {
+{
     type: "checkbox",
     message: "Selected Modules",
     name: "modules",
     choices: [
-      new inquirer.Separator("The usual:"),
-      {
-        name: "Sass"
-      },
-      {
-        name: "HTML",
-        checked: true
-      },
-      {
-        name: "Javascript"
-      },
-      new inquirer.Separator("The extras:"),
-      {
-        name: "Grunt",
-      },
-      {
-        name: "Gulp"
-      },
-      {
-        name: "Jekyll",
-        disabled: "coming soon"
-      }
+        new inquirer.Separator("The usual:"),
+        {
+            name: "Sass"
+        },
+        {
+            name: "HTML",
+            checked: true
+        },
+        {
+            name: "Javascript"
+        },
+        new inquirer.Separator("The extras:"),
+        {
+            name: "Grunt",
+        },
+        {
+            name: "Gulp"
+        },
+        {
+            name: "Jekyll",
+            disabled: "coming soon"
+        }
     ],
-    validate: function( answer ) {
-      if ( answer.length < 1 ) {
-        return "You must choose at least one module.";
-      }
-      return true;
+    validate: function (answer) {
+        if (answer.length < 1) {
+            return "You must choose at least one module.";
+        }
+        return true;
     }
-  }
-], function( answers ) {
+}], function (answers) {
     var mainAnswers = answers;
 
+    if (optionPicked(answers.modules, "Grunt") || optionPicked(answers.modules, "Gulp")) {
 
-
-    if (optionPicked(answers.modules,"Grunt") || optionPicked(answers.modules,"Gulp")) {
-
-        console.log('');
-        console.log('Creating Project Config'.green);
-        console.log( '' );
-
-        inquirer.prompt( gruntQuestions, function( answers ) {
-            console.log( '' );
-            console.log( 'Creating package.json'.green);
-            console.log( '' );
-
-            console.log(JSON.stringify(answers, null, "  ") );
-
-            var stream = fs.createWriteStream("package.json");
-
-            var preferences = {
-                "name" : answers.project,
-                "version" : answers.version,
-                "description" : answers.description
+        fs.exists("package.json", function (exists) {
+            if (exists) {
+                inquirer.prompt({
+                    type: "confirm",
+                    name: "confirm",
+                    message: "Do you want to overwrite the existing package.json ?"
+                }, function (answer) {
+                    if (answer.confirm) {
+                        createPackageInfo(gruntQuestions, answers, mainAnswers);
+                    } else {
+                        createModules(mainAnswers.modules);
+                    }
+                });
+            } else {
+                createPackageInfo(gruntQuestions, answers, mainAnswers);
             }
-
-
-            stream.write(JSON.stringify(preferences, null, "  "));
-            stream.end();
-
-
-            createModules(mainAnswers.modules);
         });
+
 
     } else {
         createModules(mainAnswers.modules);
@@ -109,35 +98,62 @@ inquirer.prompt([
 
 });
 
-function optionPicked(array,option) {
+function createTitle(title, color) {
+    console.log('');
+    if (color == 'green') {
+        console.log(title.green);
+    } else {
+        console.log(title);
+    }
+    console.log('');
+}
+
+function optionPicked(array, option) {
     return array.indexOf(option) > -1;
 }
 
-function createModules(modules,ignore) {
+function createModules(modules, ignore) {
 
-    console.log('');
-    console.log('Creating Modules'.green);
-    console.log('');
+    createTitle('Creating Modules', 'green');
 
 
-    for (var i = 0; i < modules.length; i++ ) {
+    for (var i = 0; i < modules.length; i++) {
 
         if (modules[i].toLowerCase() != ignore) {
             ncp(modulesPath() + modules[i], process.cwd(), function (err) {
                 if (err) {
-                 return console.error(err);
+                    return console.error(err);
                 }
             });
-            console.log(modulesPath() + modules[i] + ' ---> ' + process.cwd() );
+            console.log(modulesPath() + modules[i] + ' ---> ' + process.cwd());
         }
 
     }
 
     console.log('');
-
-
 }
 
+function createPackageInfo(questions, answers, mainAnswers) {
+    createTitle('Creating Project Config', 'green');
+
+    inquirer.prompt(questions, function (answers) {
+        createTitle('Creating package.json', 'green');
+
+        console.log(JSON.stringify(answers, null, "  "));
+
+        var preferences = {
+            "name": answers.project,
+            "version": answers.version,
+            "description": answers.description
+        }
+
+        var stream = fs.createWriteStream("package.json");
+        stream.write(JSON.stringify(preferences, null, "  "));
+        stream.end();
+
+        createModules(mainAnswers.modules);
+    });
+}
 
 function modulesPath() {
     return localPath() + modulesLocal;
