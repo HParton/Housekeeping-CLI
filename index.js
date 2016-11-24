41bc581041ffac0558d167a9c44024733d3bf1fc
Harry Parton <hparton@hotmail.co.uk>
2016-11-24 11:57:55 +0000

    Add first time running script to install default builds

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ var pkg = require('./package.json');
 var conf = new Configstore(pkg.name, {builds: []});
 
 program
-  .version('0.2.6')
+  .version('1.1.0')
   .option('add [build]', 'Add a npm hosted build generator')
   .option('remove [build]', 'Remove a npm hosted  build generator')
   .option('update [build]', 'Update a npm hosted  build generator')
+  .option('list', 'List all currently installed packages')
   .option('-v, --verbose', 'Enable extra information and errors')
   .option('-d, --debug', 'Enable extra information for debugging')
   .parse(process.argv);
@@ -34,35 +35,7 @@ if (program.add) {
 
   if (typeof program.add == 'string') {
     info('Finding package for ' + program.add + '...');
-
-    run('npm install ' + program.add, {cwd: buildsDir}, function() {
-      run('npm install', {cwd: buildsDir + '/node_modules/' + program.add}, function() {
-        success(program.add + ' dependencies installed.');
-      })
-
-      run('npm view ' + program.add, {cwd: buildsDir}, function(stdout) {
-        var details = eval('(' + stdout + ')');
-
-        var build = {
-          name: details.name,
-          description: details.description,
-          version: details.version,
-          maintainers: details.maintainers
-        }
-
-        if (program.verbose) {
-          console.log(build);
-        }
-
-        var currentBuilds = conf.get('builds');
-        currentBuilds.push(build);
-        conf.set('builds', currentBuilds);
-
-        success(program.add + ' added.');
-      })
-    });
-
-
+    addPackage(program.add, true);
   } else {
     warn('Please select a package to add');
   }
@@ -104,18 +77,23 @@ if (program.update) {
 if (program.remove) {
 
   if (typeof program.remove == 'string') {
-    console.log('~ '.yellow + 'Removing package ' + program.remove + ' from housekeeping...');
-    run('npm remove ' + program.remove, {cwd: buildsDir}, function() {
-      var builds = conf.get('builds');
-      var index = builds.map(function(build) {
-        return build.name;
-      }).indexOf(program.remove);
+    if (selectBuildByName(program.remove, conf.get('builds')) !== undefined) {
+      info('Removing package ' + program.remove + ' from housekeeping...');
+      run('npm remove ' + program.remove, {cwd: buildsDir}, function() {
+        var builds = conf.get('builds');
+        var index = builds.map(function(build) {
+          return build.name;
+        }).indexOf(program.remove);
 
-      builds.splice(index, 1);
-      conf.set('builds', builds);
+        builds.splice(index, 1);
+        conf.set('builds', builds);
 
-      success(program.remove + ' removed.');
-    });
+        success(program.remove + ' removed.');
+      });
+    } else {
+      warn('That package is not installed.')
+    }
+
   } else {
     warn('Please select a package to remove');
   }
@@ -123,16 +101,36 @@ if (program.remove) {
   return;
 }
 
-if (!program.update && !program.add && !program.remove) {
-  info('Finding installers config files...')
+if (program.list) {
+  var names = getAllBuildNames(conf.get('builds'));
+
+  for (var i = 0; i < names.length; i++) {
+    info(names[i]);
+  }
+}
+
+if (!program.update && !program.add && !program.remove && !program.list) {
+  if (conf.get('setup_complete')) {
+    info('Finding installers config files...')
+  }
   var builds = getAllBuilds();
 
   if (builds.length) {
     selectBuildPrompt(builds);
   } else {
-    warn('Couldn\'t find any builds, make sure you added some with [hk add]');
+    if (!conf.get('setup_complete')) {
+      info('First time running, grabbing default builds...');
+
+      addPackage('housekeeping-static', true, function() {
+        conf.set('setup_complete', true);
+        selectBuildPrompt(getAllBuilds());
+      });
+    } else {
+      warn('Couldn\'t find any builds, make sure you added some with [hk add]');
+    }
   }
 }
+
 
 
 function run(cmd, location, cb){
@@ -205,6 +203,42 @@ function info(string) {
 
 function warn(string) {
   console.log('✘ '.red + string);
+}
+
+function addPackage(package, verbose, cb) {
+    run('npm install ' + package, {cwd: buildsDir}, function() {
+      if (verbose) {
+        success(package + ' installed.');
+      }
+
+      run('npm install', {cwd: buildsDir + '/node_modules/' + package}, function() {
+        success(package + ' dependencies installed.');
+
+        run('npm view ' + package, {cwd: buildsDir}, function(stdout) {
+          var details = eval('(' + stdout + ')');
+
+          var build = {
+            name: details.name,
+            description: details.description,
+            version: details.version,
+            maintainers: details.maintainers
+          }
+
+          if (program.verbose) {
+            console.log(build);
+          }
+
+          var currentBuilds = conf.get('builds');
+          currentBuilds.push(build);
+          conf.set('builds', currentBuilds);
+
+          if (cb) {
+            cb();
+          }
+        })
+
+      })
+    });
 }
 
 function selectBuildByName(name, builds) {
